0fef45a29a99dcb5ea0a6778dd856a933426e5f6
LiaoZiqi-GZFLS <503546675@qq.com>
2025-07-25 03:46:51 +0800

    push release

--- challenge/src/client.rs
+++ challenge/src/client.rs
@@ -14,7 +14,11 @@ pub(crate) struct Client {
 impl Client {
     // Create a new client with a grid of size m * n
     pub(crate) fn new(m: u32, n: u32) -> Self {
-        let config = ConfigBuilder::default().use_custom_parameters(V1_3_PARAM_MULTI_BIT_GROUP_3_MESSAGE_2_CARRY_2_KS_PBS_GAUSSIAN_2M64).build();
+        let config = if m < 3 && n < 3 {
+            ConfigBuilder::default().use_custom_parameters(V1_3_PARAM_MULTI_BIT_GROUP_3_MESSAGE_2_CARRY_2_KS_PBS_GAUSSIAN_2M64).build()
+        } else {
+            ConfigBuilder::default().build()
+        };
         //let config = ConfigBuilder::default().build();
         let client_key = ClientKey::generate(config);
         let server_key = ServerKey::new(&client_key);
--- challenge/src/main.rs
+++ challenge/src/main.rs
@@ -17,9 +17,8 @@ const TEST_CASES: [(u32, u32, u32, f64, u32); 9] = [
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut score = 0;
-    // 打印表头，增加结果正确性和时间是否超时的列
-    println!("    #      m      n   steps   time (s)   res   correct   time_limit");
-    println!("----- ------ ------ ------- ---------- ----- ---------  -----------");
+    println!("    #      m      n   steps   time (s)   res");
+    println!("----- ------ ------ ------- ---------- -----");
 
     for (i, (m, n, steps, threshold, sco)) in TEST_CASES.iter().enumerate() {
         let start = std::time::Instant::now();
@@ -27,28 +26,24 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         let (server_key, encrypted_grid) = client.encrypt();
         let server = server::Server::new(server_key, encrypted_grid);
 
-        // 运行服务器模拟
+        // Run the server simulation
         let result_grid = server.run(*steps);
 
-        // 验证结果
-        let correct = client.verify(result_grid, *steps);
+        // Verify the result
+        let mut pass = client.verify(result_grid, *steps);
         let duration = start.elapsed().as_secs_f64();
-        let time_limit_passed = duration <= *threshold;
 
-        let pass = correct && time_limit_passed;
+        pass &= duration <= *threshold;
         score += if pass { *sco } else { 0u32 };
 
-        // 打印每个测试用例的结果，包括结果正确性和时间是否超时
         println!(
-            "{:5} {:6} {:6} {:7} {:10.4}  {}       {}         {}",
+            "{:5} {:6} {:6} {:7} {:10.4}  {}",
             i,
             m,
             n,
             steps,
             duration,
-            if pass { "PASS" } else { "FAIL" },
-            if correct { "P" } else { "F" },
-            if time_limit_passed { "P" } else { "F" }
+            if pass { "PASS" } else { "FAIL" }
         );
     }
 
